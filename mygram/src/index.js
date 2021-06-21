e15f9491c9175e6b027ec49d6d977b098a78eeb4
Mike Carter <elite.mikecarter@gmail.com>
2021-06-21 16:11:42 -0400

    created seeed file and folders

--- mygram/src/index.js
+++ mygram/src/index.js
@@ -5,4 +5,19 @@ import App from './App';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 
+// client side rendered app: react (create)
+// connect to datebase which is Firebase
+// react -loading skeleton
+// tailwind
 
+// architecure (folder structure)
+  // src
+       // -> components, 
+       // ->constants, 
+       // -> context, 
+       // -> helpers, 
+       // -> hooks,
+       // -> pages,
+       // -> library (firebase is going to live in here)
+       // -> services (firebase functions in here)
+       // -> styles ( tailwind's folder (app/tailwind))
